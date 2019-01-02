27e8325a2e47bbe5e0243b486d1336985bf7ecf5
abhishekdass08@gmail.com <abhishekdass08@gmail.com>
2019-01-03 02:57:03 +0530

    redeploy

---
+++ precache-manifest.cbcff5026832228e8e38bafdf459d784.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/demo-cart-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "e02dfde52843421f00ae",
+    "url": "/demo-cart-react/static/css/main.e598c5f3.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/demo-cart-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a4ecce0c865a70b5fea8",
+    "url": "/demo-cart-react/static/js/1.a4ecce0c.chunk.js"
+  },
+  {
+    "revision": "9eecc30dc466ec5b1b6b",
+    "url": "/demo-cart-react/static/js/runtime~main.9eecc30d.js"
+  },
+  {
+    "revision": "e02dfde52843421f00ae",
+    "url": "/demo-cart-react/static/js/main.e02dfde5.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/demo-cart-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/demo-cart-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/demo-cart-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a4ecce0c865a70b5fea8",
+    "url": "/demo-cart-react/static/css/1.bb80141d.chunk.css"
+  },
+  {
+    "revision": "22560a1847897e0d1b35b47158ad06e7",
+    "url": "/demo-cart-react/index.html"
+  }
+];
